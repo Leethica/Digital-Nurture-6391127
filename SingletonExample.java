4f88fe74e1ddec0953e08342b68d7c860e9f32ed
Leethica <leethica.sm@gmail.com>
2025-06-19 21:13:22 +0530

    Code and output for the implementation of singleton pattern

---
+++ SingletonExample.java
@@ -0,0 +1,25 @@
+public class SingletonExample {
+private static SingletonExample instance;
+
+private SingletonExample() {
+System.out.println("Singleton instance created");
+}
+
+public static SingletonExample getInstance() {
+if (instance == null) {
+instance = new SingletonExample();
+}
+return instance;
+}
+
+public void showMessage() {
+System.out.println("Hello from Singleton!");
+}
+
+public static void main(String[] args) {
+        SingletonExample obj1 = SingletonExample.getInstance();
+        SingletonExample obj2 = SingletonExample.getInstance();
+        obj1.showMessage();
+        System.out.println("Are both instances same? " + (obj1 == obj2));
+    }
+}
