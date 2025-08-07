76903985fa6f2cca5e506e97a26573283e54a2a3
Leethica <leethica.sm@gmail.com>
2025-08-07 09:29:56 +0530

    CricketApp

--- week 7/App.js
+++ week 7/App.js
@@ -1,39 +1,19 @@
-import React, { useState } from 'react';
-import GuestPage from './GuestPage';
-import UserPage from './UserPage';
+// src/App.js
+import React from 'react';
+import ListofPlayers from './ListofPlayers';
+import IndianPlayers from './IndianPlayers';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => setIsLoggedIn(true);
-  const handleLogout = () => setIsLoggedIn(false);
-
-  // Element variables
-  let message;
-  let button;
-  let pageContent;
-
-  if (isLoggedIn) {
-    message = <h2>Welcome back</h2>;
-    button = <button onClick={handleLogout}>Logout</button>;
-    pageContent = <UserPage />;
-  } else {
-    message = <h2>Please sign up.</h2>;
-    button = <button onClick={handleLogin}>Login</button>;
-    pageContent = <GuestPage />;
-  }
+  const flag = true; // Change this to false to show IndianPlayers
 
   return (
-    <div className="App" style={{ padding: '30px', fontFamily: 'Arial', textAlign: 'center' }}>
-      <h1>Ticket Booking App</h1>
-      {message}
-      <br /><br />
-      {button}
-      <hr style={{ margin: '20px 0' }} />
-      {pageContent}
+    <div className="App">
+      <h1>Cricket App</h1>
+      {flag ? <ListofPlayers /> : <IndianPlayers />}
     </div>
   );
 }
+
 export default App;
 
 
